b0eec53f08e86a0d7e460fff5ca160123be56722
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-20 13:11:35 -0800

    Add comments

--- src/Main.hs
+++ src/Main.hs
@@ -15,18 +15,30 @@ import Text.Read (readMaybe)
 defaultPort :: Port
 defaultPort = 8080
 
+-- Read port number from the environment variable "PORT".
+
 getPort :: IO Port
-getPort = readPort `catch` useDefault where
+getPort =
+    -- `getEnv` will throw an exception if "PORT" isn't set in the
+    -- environment, so we need to catch that exception and provide a
+    -- default port.
+    readPort `catch` useDefault where
     readPort = do
         port <- getEnv "PORT"
+        -- If the value of "PORT" can't be parsed to a string, use the
+        -- default port.
         return (fromMaybe defaultPort (readMaybe port))
+    -- Catch the exception when there is no "PORT" environment variable.
     useDefault :: IOError -> IO Port
     useDefault = const (return defaultPort)
 
 -- Environment
 
+-- There are three possible runtime environments.
 data Environment = Development | Production | Test
 defaultEnvironment = Development
+
+-- Read runtime environment from the environment variable "ENV".
 
 getEnvironment :: IO Environment
 getEnvironment = readEnvironment `catch` useDefault where
@@ -43,6 +55,9 @@ getEnvironment = readEnvironment `catch` useDefault where
 loggingMiddleware :: Environment -> Middleware
 loggingMiddleware Development = logStdoutDev
 loggingMiddleware Production = logStdout
+-- Middleware are functions that take a WAI handler argument and return
+-- another WAI handler function. The `id` function just returns its
+-- argument, so it works as middleware that doesn't change the handler.
 loggingMiddleware Test = id
 
 -- Server
@@ -52,4 +67,5 @@ main = do
     port <- getPort
     environment <- getEnvironment
     putStrLn $ "Listening on port " ++ show port ++ "."
+    -- Run a Warp server using the request handler.
     run port $ loggingMiddleware environment handler
